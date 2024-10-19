0657150202c55bf2f5b2b0665d443231655a44c2
Cameron Taggart <cameron.tagg>
2024-10-18 19:20:34 -0600

    about to get voidZero.kind()

--- cowsay-test/src/cowsay-test.ts
+++ cowsay-test/src/cowsay-test.ts
@@ -3,6 +3,12 @@
 import { test, canvas } from 'cowsay';
 console.log(test());
 console.log(canvas.tsVersion());
+let options = new canvas.Compileroptions();
+console.log(options.inner());
+
+let nodeFactory = canvas.nodeFactory();
+let voidZero = nodeFactory.createVoidZero();
+console.log(voidZero.kind());
 
 // import { cow } from 'cowsay';
 // console.log(cow.tsVersion());
--- cowsay/src/cowsay.ts
+++ cowsay/src/cowsay.ts
@@ -1,12 +1,45 @@
-// import * as ts from 'typescript';
+import * as ts from 'typescript';
 export function test() {
     return "ok";
 }
 
-class CompilerOptions {
-    inner;
+class Compileroptions {
+    private _inner;
     constructor() {
-        this.inner = 3;
+        this._inner = 3;
+    }
+    inner() {
+        return this._inner;
+    }
+}
+
+class Bundle {
+    private _value : ts.Bundle;
+    constructor() {
+        this._value = new Object() as ts.Bundle;
+    }
+    kind() {
+        return this._value.kind;
+    }
+}
+
+class Nodefactory {
+    private _value : ts.NodeFactory;
+    constructor(value: ts.NodeFactory) {
+        this._value = value;
+    }
+    createVoidZero() {
+        return new Voidexpression(this._value.createVoidZero());
+    }
+}
+
+class Voidexpression {
+    private _value : ts.VoidExpression;
+    constructor(value: ts.VoidExpression) {
+        this._value = value;
+    }
+    kind() {
+        return this._value.kind;
     }
 }
 
@@ -15,7 +48,17 @@ function tsVersion() {
     return "5.2";
 }
 
+
+function nodeFactory() {
+    // would like to try to return existing types if possible
+    // return ts.factory;
+    return new Nodefactory(ts.factory);
+}
+
 export const canvas = {
     tsVersion,
-    CompilerOptions
+    Compileroptions,
+    nodeFactory,
+    Nodefactory,
+    Voidexpression,
 };
