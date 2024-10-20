52e75a3366b9556bf2b45c5435510830f6bbef25
Cameron Taggart <cataggar@microsoft.com>
2024-10-19 19:22:26 -0600

    jco + dotnet almost

--- build.nu
+++ build.nu
@@ -4,9 +4,16 @@
 # dotnet build -p host-app
 dotnet build
 
+# winget install Oven-sh.Bun
+# winget install Oven-sh.Bun.Profile
+cd cowsay
+bun x run bundle
+bun x run componentize
+
+cd ..
 (wac plug
     host-app/bin/Debug/net9.0/wasi-wasm/native/host-app.wasm
-    --plug adder/bin/Debug/net9.0/wasi-wasm/native/adder.wasm
+    # --plug adder/bin/Debug/net9.0/wasi-wasm/native/adder.wasm
     -o main.wasm)
 
 wasmtime run main.wasm
--- host-app/Program.cs
+++ host-app/Program.cs
@@ -1,6 +1,10 @@
-﻿using HostappWorld.wit.imports.example.component;
+﻿// using HostappWorld.wit.imports.example.component;
+using HostappWorld.wit.imports.local.cowsay;
 
-uint left = 1;
-uint right = 2;
-uint result = AddInterop.Add(left, right);
-Console.WriteLine($"Hello {left} + {right} = {result}");
+// uint left = 1;
+// uint right = 2;
+// uint result = AddInterop.Add(left, right);
+// Console.WriteLine($"Hello {left} + {right} = {result}");
+
+var tsVersion = CanvasInterop.TsVersion();
+Console.WriteLine($"TS versino {tsVersion}");
