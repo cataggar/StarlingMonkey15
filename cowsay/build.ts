79cc63ba7e11501110d44825a0619a51da095bed
Cameron Taggart <cameron.tagg>
2024-10-30 19:37:26 -0600

    Cannot access performance properties at pre-initialization time. https://github.com/bytecodealliance/ComponentizeJS/issues/153

--- cowsay/build.ts
+++ cowsay/build.ts
@@ -1,3 +1,6 @@
+
+import { promises as fs } from 'fs';
+
 // https://bun.sh/docs/bundler
 await Bun.build({
     entrypoints: ['./src/cowsay.ts'],
@@ -8,4 +11,13 @@ await Bun.build({
     // sourcemap: 'inline',
     sourcemap: 'none',
 });
+
+// Cannot access performance properties at pre-initialization time.
+// https://github.com/bytecodealliance/ComponentizeJS/issues/153
+// Workaround is to turn off the performance hooks by returning undefined.
+// https://github.com/microsoft/TypeScript/blob/main/src/compiler/performanceCore.ts
+let js = await fs.readFile('cowsay.js', 'utf8');
+js = js.replace('function tryGetPerformanceHooks() {', 'function tryGetPerformanceHooks() { return;');
+await fs.writeFile('cowsay.js', js, 'utf8');
+
 export {};
