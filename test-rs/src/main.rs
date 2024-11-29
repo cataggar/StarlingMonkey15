8e9d1c4c43eee5b15ba008704a56c224336494f9
Cameron Taggart <cameron.taggart@gmail.com>
2024-11-29 17:26:42 -0600

    get-arguments

--- test-rs/src/main.rs
+++ test-rs/src/main.rs
@@ -3,4 +3,5 @@ use bindings::ts::typescript_system::system;
 
 fn main() {
     println!("current directory: {}", system::get_current_directory());
+    println!("arguments: {:?}", system::get_arguments());
 }
--- typescript-system/src/lib.rs
+++ typescript-system/src/lib.rs
@@ -7,6 +7,9 @@ impl bindings::exports::ts::typescript_system::system::Guest for System {
     fn get_current_directory() -> String {
         environment::initial_cwd().unwrap_or("/none".to_string())
     }
+    fn get_arguments() -> Vec<String> {
+        environment::get_arguments()
+    }
 }
 
 bindings::export!(System with_types_in bindings);
