1d004b4080c6e82da8089a1cd608a98e884f2f83
Cameron Taggart <cameron.taggart@gmail.com>
2024-11-30 10:13:53 -0600

    put functions in a resource

--- typescript-system/src/lib.rs
+++ typescript-system/src/lib.rs
@@ -1,18 +1,24 @@
 mod bindings;
 use bindings::wasi::cli::environment;
+use bindings::exports::ts::typescript_system::ts::{Guest, GuestSystem};
+
+struct Component;
+
+impl Guest for Component {
+    type System = System;
+}
 
 struct System;
-
-impl bindings::exports::ts::typescript_system::system::Guest for System {
-    fn get_current_directory() -> String {
-        environment::initial_cwd().unwrap_or("/none".to_string())
+impl GuestSystem for System {
+    fn get_current_directory(&self) -> String {
+        environment::initial_cwd().unwrap_or("/".to_string())
     }
-    fn get_arguments() -> Vec<String> {
+    fn get_arguments(&self) -> Vec<String> {
         environment::get_arguments()
     }
-    fn get_environment() -> Vec<(String, String)> {
+    fn get_environment(&self) -> Vec<(String, String)> {
         environment::get_environment()
     }
 }
 
-bindings::export!(System with_types_in bindings);
+bindings::export!(Component with_types_in bindings);
