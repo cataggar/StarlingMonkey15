0dc20e0a3ac9051bded5b47fb760422f03aab2cc
Cameron Taggart <cameron.taggart@gmail.com>
2024-12-02 11:05:02 -0600

    put bundle back

--- typescript/bundle.ts
+++ typescript/bundle.ts
@@ -14,4 +14,9 @@ if (!build.success) {
     process.exit(1);
 }
 
+// workaround for https://github.com/oven-sh/bun/issues/15537
+let js = await fs.readFile('world.js', 'utf8');
+js = js.replace('export { System };', '');
+await fs.writeFile('world.js', js, 'utf8');
+
 export {};
