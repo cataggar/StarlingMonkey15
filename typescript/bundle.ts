5a8830ac3ded8a292089ddb704ff7ea2a37fa688
Cameron Taggart <cameron.taggart@gmail.com>
2024-12-02 21:36:58 -0600

    remove workaround

--- typescript/bundle.ts
+++ typescript/bundle.ts
@@ -8,15 +8,11 @@ let build = await Bun.build({
     minify: false,
     format: 'esm',
     sourcemap: 'none',
+    external: ['ts-typescript-system-types']
 });
 if (!build.success) {
     console.error(build);
     process.exit(1);
 }
-
-// workaround for https://github.com/oven-sh/bun/issues/15537
-let js = await fs.readFile('world.js', 'utf8');
-js = js.replace('export { System };', '');
-await fs.writeFile('world.js', js, 'utf8');
 
 export {};
