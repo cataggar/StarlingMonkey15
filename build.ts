bef47eb3e50ed05f38a7d2bf6b9db750170640e4
Cameron Taggart <cameron.taggart@gmail.com>
2024-12-16 19:48:27 -0600

    have it create "todo"

--- build.ts
+++ build.ts
@@ -9,18 +9,19 @@ async function runScript(cwd: string, script: string) {
     }
 }
 
-await runScript("typescript-system", "wkg");
-await runScript("typescript-system", "cargo");
+// await runScript("typescript-system", "wkg");
+// await runScript("typescript-system", "cargo");
 
-await runScript("typescript", "wkg");
-await runScript("typescript", "types");
-// await runScript("typescript", "tsc");
-await runScript("typescript", "bundle");
-await runScript("typescript", "componentize");
+// await runScript("typescript", "wkg");
+// await runScript("typescript", "types");
+// // await runScript("typescript", "tsc");
+// await runScript("typescript", "bundle");
+// await runScript("typescript", "componentize");
 
 await runScript("test-rs", "wkg");
 await runScript("test-rs", "cargo");
 await runScript("test-rs", "plug");
+await runScript("test-rs", "plug2");
 await runScript("test-rs", "run");
 
 export {};
