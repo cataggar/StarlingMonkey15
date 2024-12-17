7d4fa90f3df2fb9c5d114ab893b22c4f4211c8d9
Cameron Taggart <cameron.taggart@gmail.com>
2024-12-16 22:47:12 -0600

    this breaks

--- typescript/src/world.ts
+++ typescript/src/world.ts
@@ -4,8 +4,8 @@ class Program {
     constructor(public system: System){}
     concatArguments() {
         // return "todo";
-        // return this.system.getArguments().concat("__");
-        return this.system.hello();
+        return this.system.getArguments().concat("__");
+        // return this.system.hello();
     }
 }
 
